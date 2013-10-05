1aa49ec8d67b42b79d6a452ccdf11c97b8e628d4
Boris Doesborg <bdoesborg@gmail.com>
2013-10-05 21:15:57 +0200

    remove flickr iframe. add flickr js thumbnails

--- docroot/js/main.js
+++ docroot/js/main.js
@@ -1,1 +1,39 @@
+$(document).ready(function(){
+  
+  // Our very special jQuery JSON fucntion call to Flickr, gets details
 
+// of the most recent 20 images
+
+// flickr id batigollix 62546836@N00
+
+//$.getJSON("http://api.flickr.com/services/feeds/groups_pool.gne?id=998875@N22&lang=en-us&format=json&jsoncallback=?", displayImages);
+$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=62546836@N00&tags=max&lang=en-en&format=json&jsoncallback=?", displayImages);
+
+function displayImages(data) {
+
+    // Start putting together the HTML string
+    var htmlString = "<ul>";
+    
+    // Now start cycling through our array of Flickr photo details
+    $.each(data.items, function(i,item){
+        if(i < 10) {
+    
+        // I only want the ickle square thumbnails
+        var sourceSquare = (item.media.m).replace("_m.jpg", "_s.jpg");
+        
+        // Here's where we piece together the HTML
+        htmlString += '<li><a href="' + item.link + '" target="_blank">';
+        htmlString += '<img title="' + item.title + '" src="' + sourceSquare;
+        htmlString += '" alt="'; htmlString += item.title + '" />';
+        htmlString += '</a></li>';
+        
+        }    
+    });
+    
+    // Pop our HTML in the #images DIV
+    $('#images').html(htmlString + "</ul>");
+    
+    // Close down the JSON function call
+}
+
+});
